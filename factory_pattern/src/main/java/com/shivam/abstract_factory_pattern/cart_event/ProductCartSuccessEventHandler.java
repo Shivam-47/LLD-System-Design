38c2be41419a97179824f98de250f082d60b7eb2
--add <sh17@iitbbs.ac.in>
2025-01-28 00:54:04 +0530

    minor changes

--- factory_pattern/src/main/java/com/shivam/abstract_factory_pattern/cart_event/ProductCartSuccessEventHandler.java
+++ factory_pattern/src/main/java/com/shivam/abstract_factory_pattern/cart_event/ProductCartSuccessEventHandler.java
@@ -1,6 +1,6 @@
 package com.shivam.abstract_factory_pattern.cart_event;
 
-public class ProductSuccessEventHandler implements CartActionHandler {
+public class ProductCartSuccessEventHandler implements CartActionHandler {
     @Override
     public void HandleEvent() {
         System.out.println("Handles ProductSuccessEventHandler");
--- factory_pattern/src/main/java/com/shivam/abstract_factory_pattern/cart_type/ProductCartEventHandler.java
+++ factory_pattern/src/main/java/com/shivam/abstract_factory_pattern/cart_type/ProductCartEventHandler.java
@@ -2,14 +2,14 @@ package com.shivam.abstract_factory_pattern.cart_type;
 
 import com.shivam.abstract_factory_pattern.cart_event.CartActionHandler;
 import com.shivam.abstract_factory_pattern.cart_event.ProductCartFailedEventHandler;
-import com.shivam.abstract_factory_pattern.cart_event.ProductSuccessEventHandler;
+import com.shivam.abstract_factory_pattern.cart_event.ProductCartSuccessEventHandler;
 
 public class ProductCartEventHandler implements CartTypeEventHandler {
     @Override
     public CartActionHandler getActionHandler(String eventType){
         switch (eventType){
             case "success":
-                return new ProductSuccessEventHandler();
+                return new ProductCartSuccessEventHandler();
             case "failed":
                 return new ProductCartFailedEventHandler();
             default:
